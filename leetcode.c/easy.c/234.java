16f826f31f8c40e1a5e53c54f85bfc2729001ec5
王育杰 <37700859+453594848@users.noreply.github.com>
2022-03-13 17:05:54 +0800

    Create 234.java

---
+++ leetcode.c/easy.c/234.java
@@ -0,0 +1,27 @@
+class Solution {
+ public static boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) {
+            return true;
+        }
+        ListNode slow = head, fast = head;
+        ListNode pre = null;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            ListNode temp = slow.next;
+            slow.next = pre;
+            pre = slow;
+            slow = temp;
+        }
+        if(fast != null) {
+            slow = slow.next;
+        }
+        while(pre != null && slow != null) {
+            if(pre.val != slow.val) {
+                return false;
+            }
+            pre = pre.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
