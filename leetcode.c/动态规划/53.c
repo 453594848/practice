f425f1089eed40d190a2158a892c5ec498179680
453594848 <37700859+453594848@users.noreply.github.com>
2023-11-20 18:58:07 +0800

    Delete leetcode.c directory

--- leetcode.c/动态规划/53.c
+++
@@ -1,8 +0,0 @@
-int maxSubArray(int* nums, int numsSize){
-    int max=nums[0],sum;
-for(int i=0;i<numsSize;i++){
-sum=fmax(nums[i],sum+nums[i]);
-max=fmax(sum,max);
-}
-return max;
-}
--- leetcode.c/动态规划/70.c
+++
@@ -1,9 +0,0 @@
-int climbStairs(int n){
-    int p = 0, q = 0, m = 1;
-    for (int i = 1; i <= n; i++) {
-        p = q;
-        q = m;
-        m = p + q;
-    }
-    return m;
-}
